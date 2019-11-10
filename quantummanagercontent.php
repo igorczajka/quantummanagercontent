0b57f8ecd2a9a78248dcbe620ad80452718d158f
igorczajka <igor@czajka.art.pl>
2019-11-10 20:47:51 +0100

    Update quantummanagercontent.php
    add vars to file template

--- quantummanagercontent.php
+++ quantummanagercontent.php
@@ -147,9 +147,15 @@ EOT
 
 		$scope = $data['scope'];
 		$file = QuantummanagerHelper::preparePath($data['path'], false, $scope, true);
+		$name = explode('/', $file);
+		$filename = end($name);
+        $filesize = filesize(JPATH_SITE."/".$file);
 		$scopesTemplate = $this->params->get('scopes', QuantummanagercontentHelper::defaultValues());
 		$variables = [
 			'{file}' => $file,
+            '{filename}' => $filename,
+            '{type}' => substr($file, -3),
+            '{size}' => $this->convert_filesize($filesize),
 		];
 
 		foreach ($data as $key => $value)
@@ -197,5 +203,10 @@ EOT
 		$app->close();
 	}
 
+    public function convert_filesize($bytes, $decimals = 2){
+        $size = array('b','kB','MB','GB','TB','PB','EB','ZB','YB');
+        $factor = floor((strlen($bytes) - 1) / 3);
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . " " . @$size[$factor];
+    }
 
 }
